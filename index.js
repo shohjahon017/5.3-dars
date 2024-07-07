0baa31e0b809f7a7dbd4a753ef658bdf567d2e0a
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-07-07 17:50:14 +0500

    ..

--- index.js
+++ index.js
@@ -10,6 +10,23 @@
 
 //String metodlariga oid masalalar.
 //1-masala
+// function substringElement(str) {
+//   return str.substring(0, 5);
+// }
+// console.log(substringElement("Hello, world!"));
+
+//2-masala
+// function lowerCaseElement(str) {
+//   let res = str.split("").map((element) => {
+//     if (element === element.toLowerCase()) {
+//       return element.toUpperCase();
+//     } else {
+//       return element.toLowerCase();
+//     }
+//   });
+//   return res.join("");
+// }
+// console.log(lowerCaseElement("Hello, WORLD!"));
 
 // //3-masala
 // function createGapElement(str) {
@@ -19,13 +36,43 @@
 // console.log(createGapElement("Hello,world,from, JavaScript"));
 
 //4-masala
+// function joinElement(str) {
+//   return str.join(",");
+// }
+// console.log(joinElement(["Hello", "world", "from", "JavaScript"]));
+
+//5-masala
 // function replaceElement(str) {
 //   return str.replace(/a/g, "@");
 // }
 // console.log(replaceElement("JavaScript"));
 
-//5-masala
-function removeGapElement(str) {
-  return str.trim();
-}
-console.log(removeGapElement("   Hello, world!   "));
+//6-masala
+// function removeGapElement(str) {
+//   return str.trim();
+// }
+// console.log(removeGapElement("   Hello, world!   "));
+
+//7-masala
+// function findElement(str) {
+//   return str.charAt(7);
+// }
+// console.log(findElement("Hello, world!"));
+
+//8-masala
+// function isExistElement(str) {
+//   return str.includes("world");
+// }
+// console.log(isExistElement("Hello, world"));
+
+//9-masala
+// function findElement(str) {
+//   return str.startsWith("Hello");
+// }
+// console.log(findElement("Hello, world"));
+
+//10-masala
+// function endsWithElement(str) {
+//   return str.endsWith("world!");
+// }
+// console.log(endsWithElement("Hello, world!"));
