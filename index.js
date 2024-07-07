3810e0d42da9286f9594a20a2296bb289c0a101e
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-07-07 17:31:11 +0500

    .

---
+++ index.js
@@ -0,0 +1,31 @@
+// Massiv va obyektlarga oid masalalar.
+//1-masala
+// const students = [
+//   { name: "Alice", age: 20, score: 85, status: "active" },
+//   { name: "Bob", age: 22, score: 92, status: "inactive" },
+// ];
+// function nameStudent(arr) {
+//   console.log();
+// }
+
+//String metodlariga oid masalalar.
+//1-masala
+
+// //3-masala
+// function createGapElement(str) {
+//   let res = str.split(" ");
+//   return res.join(" ");
+// }
+// console.log(createGapElement("Hello,world,from, JavaScript"));
+
+//4-masala
+// function replaceElement(str) {
+//   return str.replace(/a/g, "@");
+// }
+// console.log(replaceElement("JavaScript"));
+
+//5-masala
+function removeGapElement(str) {
+  return str.trim();
+}
+console.log(removeGapElement("   Hello, world!   "));
